25bec45abc878802da5bbe44a1b180326651620d
mosarof66 <devmosarof.123@gmail.com>
2025-11-08 21:51:08 +0600

    cron job added for reseting the daylimit at 5am daily

---
+++ src/app/api/reset-limit/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import User from '@/models/user.model'
+import { connectDB } from '@/db/db'
+
+
+await connectDB();
+
+
+
+export async function GET() {
+    try {
+
+        console.log('reset limit called..')
+
+        await User.updateMany({}, { $set: { dayLimit: 0 } })
+        console.log('reset limit done..')
+        return NextResponse.json({
+            message: "All users day limit reseted successfully. ",
+            success: true
+        }, { status: 201 })
+
+    } catch (error) {
+        console.log("error while calling this reset limit function : ", error)
+        return NextResponse.json({
+            message: "Internal server error ",
+            success: false
+        }, { status: 500 })
+    }
+}
